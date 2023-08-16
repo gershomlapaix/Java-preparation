9e8d6a9b4e5d8f77a191fafefa92c7224052f605
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-16 10:50:45 +0200

    finish file handling

--- Filehandling/src/WriteToFile.java
+++ Filehandling/src/WriteToFile.java
@@ -1,17 +1,31 @@
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WriteToFile {
     public void writeToFile(){
+        String[] texts = {"Here is file handling in java.","Every Java developer should know."};
+
+        /*
         try(FileWriter writer = new FileWriter("example.txt")){
-            String[] texts = {"Here is file handling in java.\n","Every Java developer should know."};
             int x = 0;
            while(x < texts.length){
                writer.write(texts[x]);
                x++;
             }
             System.out.println("done writing to a file");
-        }catch (IOException io){
+        }
+         */
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("example.txt"))){
+            int x = 0;
+            while(x < texts.length) {
+                writer.write("Hello, bufferedWriter!");
+                writer.newLine();    // move to the next line
+                x++;
+            }
+        }
+        catch (IOException io){
             System.out.println("An error occured : "+io.getMessage());
         }
     }
