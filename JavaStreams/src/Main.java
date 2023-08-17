f2ca06883d32fb5a39f82c49c1c180d85d8e9af2
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-17 10:10:48 +0200

    school organization

--- JavaStreams/src/Main.java
+++ JavaStreams/src/Main.java
@@ -1,5 +1,53 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        using Stream API
+        List<String> strings = List.of("I", "am", "a", "list", "of", "strings");
+
+
+
+//        long result = limit.count();  // calling the count terminal operator
+//        System.out.println("The result is "+result);
+
+        /*
+        * ----How the streams work----
+        * 1. Get the Stream from a source collection ==> .stream()
+        * 2. Call zero or more "intermediate operations" on the stream     ==> .limit()
+        * 3. Output the results with a "terminal operation" ==> .collect()
+        *
+        */
+
+        /*
+        Stream<String> stream = strings.stream();
+        Stream<String> limit = stream.limit(4);
+        List<String> result = limit.collect(Collectors.toList());
+        System.out.println(result);*/
+
+//        by chaining
+        List<String> result = strings.stream()
+                .limit(4)
+                .collect(Collectors.toList());
+        System.out.println(result);
+
+//        sorted result
+        List<String> sortedResult = strings.stream()
+                .sorted()
+                .limit(4)
+                .collect(Collectors.toList());
+        System.out.println(sortedResult);
+
+//        less natural sorting
+        List<String> result2 = strings.stream()
+                .sorted((str1, str2)->str1.compareToIgnoreCase(str2))
+                .limit(4)
+                .collect(Collectors.toList());
+        System.out.println(result2);
+
+//        other more examples
+        System.out.println("\n-----More examples----");
+        new MoreExample().moreExamples();
     }
 }
--- JavaStreams/src/MoreExample.java
+++ JavaStreams/src/MoreExample.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class MoreExample {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MoreExample {
+    public void moreExamples(){
+        List<String> names = Arrays.asList("Alice", "Bob", "Anna","Alex", "John");
+
+//        filter out the names that start with the letter "A"
+        List<String> filteredNames = names.stream()
+                .filter(name -> name.startsWith("A"))
+                .collect(Collectors.toList());
+        System.out.println(filteredNames);
+
+//        square each number
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        List<Integer> squareNumbers = numbers.stream()
+                .map(number -> number * number)
+                .collect(Collectors.toList());
+        System.out.println(squareNumbers);
+
+//        get the sum of numbers using "reduce" method
+        int sum = numbers.stream()
+                .reduce(0, (a,b)-> a+b);
+        System.out.println("Sum : "+sum);
+
+//        parallel processing for large datasets
+        int sumOfSquares = IntStream.rangeClosed(1,10)
+                .parallel()
+                .map(i ->i*i)
+                .sum();
+
+        System.out.println("Sum of squares: " + sumOfSquares); // Output: Sum of squares: 385
+    }
 }
