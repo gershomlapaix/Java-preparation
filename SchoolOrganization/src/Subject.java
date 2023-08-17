b5aaa9c9000847da977d933cedd8bc6ecfde4faf
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-17 10:14:24 +0200

    subject

---
+++ SchoolOrganization/src/Subject.java
@@ -0,0 +1,16 @@
+public class Subject {
+    private String name;
+
+    public Subject(String name){
+        this.name = name;
+    }
+
+//    getters and setters
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
