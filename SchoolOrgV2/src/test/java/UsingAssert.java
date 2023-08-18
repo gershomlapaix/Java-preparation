42b99c846f39e0f7da216d5b678bfba9e983f904
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-18 11:44:10 +0200

    using assert class

--- SchoolOrgV2/src/test/java/UsingAssert.java
+++ SchoolOrgV2/src/test/java/UsingAssert.java
@@ -1,2 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class UsingAssert {
+
+    @Test
+    public void testAdd(){
+        int num = 5;
+        String temp = null;
+        String str = "working on stuffs.";
+
+
+//        check for equality
+        assertEquals("working on stuffs.", str);
+
+//        check for false condition
+        assertFalse(num > 6);
+
+//        check for null value
+        assertNotNull(temp);    // this will fail
+    }
 }
