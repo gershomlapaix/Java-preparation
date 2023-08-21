ee89e3a34df08b98cb869bab7bdc253f6bf11367
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-21 11:56:58 +0200

    define song class

--- DataStructures/src/Song.java
+++ DataStructures/src/Song.java
@@ -1,3 +1,46 @@
+// A class on which to carry out collection operations
 public class Song {
-    
+
+//    properties
+    private String title;
+    private String artist;
+    private int bpm;
+
+//    constructor
+    Song(String title, String artist, int bpm){
+        this.title = title;
+        this.artist = artist;
+        this.bpm = bpm;
+    }
+
+//    getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getBpm() {
+        return bpm;
+    }
+
+    public void setBpm(int bpm) {
+        this.bpm = bpm;
+    }
+
+//    override to string
+
+    public String toString(){
+        return title;
+    }
 }
