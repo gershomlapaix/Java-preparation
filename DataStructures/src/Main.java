6e6239da9abec004414d19e3cac0eaa4665ef706
gershomlapaix <nsengiyumv33@gmail.com>
2023-08-22 10:07:43 +0200

    maps

--- DataStructures/src/Main.java
+++ DataStructures/src/Main.java
@@ -1,7 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
 
 //        test the collections
         new JukeBox().go();
+
+//        Maps
+        System.out.println("\nMaps\n----");
+        Map<String, Integer> scores = new HashMap<>();
+
+        scores.put("Kathy", 42);
+        scores.put("Bert", 342);
+        scores.put("Skyler", 420);
+
+        System.out.println(scores);
+        System.out.println("Bert got "+ scores.get("Bert"));
     }
 }
